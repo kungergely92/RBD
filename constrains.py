485a6a7e7683cca11f878c36b7bb2a1ff3399f04
kungergely92 <kun.gergely.1992@gmail.com>
2017-12-21 18:25:17 +0100

    mechanism class

--- constrains.py
+++ constrains.py
@@ -1,7 +1,11 @@
 import sympy
 from rigid_body import RigidBody
+from mechanism import Mechanism
 
-class Constrains(RigidBody):
-    """Here are the defined constraint functions defined."""
-    def perpendicular(self):
+#class Constrains(RigidBody):
+#    """Here are the defined constraint functions defined."""
+#    def __init__(self):
+
+
+#    def perpendicular(self):
 
--- mechanism.py
+++ mechanism.py
@@ -3,6 +3,8 @@
 class Mechanism(object):
     """Mechanism class, consisting RigidBody, and Constraint objects"""
     rigid_body_list = []
+    constraint_list = []
 
     def __init__(self):
         Mechanism.rigid_body_list.append(self)
+        Mechanism.constraint_list.extend(self.constraints)
